8544e77352556dd293e2f0014ea33001a9059cc4
Olexandr Stetsyuk <san9istec@gmail.com>
2020-09-22 19:04:45 +0300

    change imports

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <img
+          src="https://webstockreview.net/images/twitter-logo-png-4.png"
+          alt="logo"
+        />
       </header>
+      <nav>
+        <div>Profile</div>
+        <div>Messages</div>
+        <div>News</div>
+        <div>Music</div>
+        <div>Settings</div>
+      </nav>
+      <div>
+        Ava+description
+        <div>
+          Posts
+          <div>
+            My posts
+            <div>post 1</div>
+            <div>post 2</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
