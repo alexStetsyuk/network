046ee54c3335caf7d444c8b07ce3132779a39c2c
alexStetsyuk <san9istec@gmail.com>
2020-09-23 15:24:27 +0300

    add css and some new content to App.js html structure

--- src/App.js
+++ src/App.js
@@ -1,30 +1,39 @@
-import React from "react"
+import React from "react";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header>
+    <div className="app-container">
+      <header className="header">
         <img
           src="https://webstockreview.net/images/twitter-logo-png-4.png"
           alt="logo"
         />
       </header>
-      <nav>
-        <div>Profile</div>
-        <div>Messages</div>
-        <div>News</div>
-        <div>Music</div>
-        <div>Settings</div>
-      </nav>
-      <div>
-        Ava+description
-        <div>
-          Posts
+      <div className="flex-wrap">
+        <nav className="navbar">
+          <div>Profile</div>
+          <div>Messages</div>
+          <div>News</div>
+          <div>Music</div>
+          <div>Settings</div>
+        </nav>
+        <div className="content">
           <div>
-            My posts
-            <div>post 1</div>
-            <div>post 2</div>
+            <img
+              src="https://images.unsplash.com/photo-1484417894907-623942c8ee29?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80"
+              alt="banner"
+              className="banner"
+            />
+          </div>
+          Ava+description
+          <div>
+            Posts
+            <div>
+              My posts
+              <div>post 1</div>
+              <div>post 2</div>
+            </div>
           </div>
         </div>
       </div>
