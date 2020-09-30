7e56ccc15552a8421defc5f11bfda4023fb5b1de
Olexandr Stetsyuk <san9istec@gmail.com>
2020-09-30 18:24:50 +0300

    add some styles to Dialog component

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -1,7 +1,22 @@
 import React from "react";
+import styles from "./Dialogs.module.css";
 
 const Dialogs = () => {
-  return <div>Dialogs</div>;
+  return (
+    <div className={styles.dialogs}>
+      <div className={styles.userDialog}>
+        <div className={styles.active}>Sofiya</div>
+        <div>Masha</div>
+        <div>Andriy</div>
+        <div>Yarik</div>
+      </div>
+      <div className={styles.dialog}>
+        <div>Hi</div>
+        <div>How are you?</div>
+        <div>How is it going?</div>
+      </div>
+    </div>
+  );
 };
 
 export default Dialogs;
