e891d5e7d3308331ddcf626d2f2041a2d0e5c647
Olexandr Stetsyuk <san9istec@gmail.com>
2020-10-02 11:04:09 +0300

    change div's to NavLink in Dialogs

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -1,14 +1,31 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import styles from "./Dialogs.module.css";
 
 const Dialogs = () => {
   return (
     <div className={styles.dialogs}>
       <div className={styles.userDialog}>
-        <div className={styles.active}>Sofiya</div>
-        <div>Masha</div>
-        <div>Andriy</div>
-        <div>Yarik</div>
+        <div>
+          <NavLink to="/dialogs/1" activeClassName={styles.active}>
+            Sofiya
+          </NavLink>
+        </div>
+        <div>
+          <NavLink to="/dialogs/2" activeClassName={styles.active}>
+            Masha
+          </NavLink>
+        </div>
+        <div>
+          <NavLink to="/dialogs/3" activeClassName={styles.active}>
+            Andriy
+          </NavLink>
+        </div>
+        <div>
+          <NavLink to="/dialogs/4" activeClassName={styles.active}>
+            Yarik
+          </NavLink>
+        </div>
       </div>
       <div className={styles.dialog}>
         <div>Hi</div>
